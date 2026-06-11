55105d8fc9e2bb39448c9866f6287996590e0091
Priyanshu <pv254424@gmail.com>
2026-06-11 19:41:00 +0530

    mandate response are updated

---
+++ NPCI/NPCI/src/main/java/com/npci/model/response/MandateResponse.java
@@ -0,0 +1,33 @@
+package com.npci.model.response;
+
+import com.npci.model.enums.MandateStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MandateResponse {
+
+    private String mandateId;
+    private String npciMandateId;
+    private String userUpiId;
+    private String merchantUpiId;
+    private String merchantName;
+    private BigDecimal maxAmount;
+    private String frequency;
+    private Integer executionDay;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalDate nextExecutionDate;
+    private MandateStatus status;
+    private String purpose;
+    private LocalDateTime createdAt;
+}

03625c0e08b4f86fcb2328158cde40a8798c17ee
Priyanshu <pv254424@gmail.com>
2026-06-23 00:53:58 +0530

    feat: add AlertNotFoundException for handling missing alert cases

---
+++ Aml/Aml/src/main/java/com/upimesh/aml/exception/AlertNotFoundException.java
@@ -0,0 +1,7 @@
+package com.upimesh.aml.exception;
+
+public class AlertNotFoundException extends RuntimeException {
+    public AlertNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ Aml/Aml/src/main/java/com/upimesh/aml/exception/ScreeningNotFoundException.java
@@ -0,0 +1,7 @@
+package com.upimesh.aml.exception;
+
+public class ScreeningNotFoundException extends RuntimeException {
+    public ScreeningNotFoundException(String message) {
+        super(message);
+    }
+}

6e686f7e3c8aae62ed2b6bbb4624beb0be8eb872
Priyanshu <pv254424@gmail.com>
2026-06-23 00:55:17 +0530

    feat: initialize AmlServiceApplication with Spring Boot, Feign, and scheduling support

---
+++ Aml/Aml/src/main/java/com/upimesh/aml/AmlServiceApplication.java
@@ -0,0 +1,19 @@
+package com.upimesh.aml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCaching
+@EnableScheduling
+public class AmlServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AmlServiceApplication.class, args);
+    }
+}

5f8b4f192885b0758a3aba9078f7d4452cd39bed
Priyanshu <pv254424@gmail.com>
2026-06-23 23:17:14 +0530

    feat: add TransactionResponse DTO for dispute feign client

---
+++ Dispute/Dispute/src/main/java/com/upimesh/dispute/feign/dto/TransactionResponse.java
@@ -0,0 +1,29 @@
+package com.upimesh.dispute.feign.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionResponse {
+    private String transactionId;
+    private String npciTransactionId;
+    private String rrn;
+    private String senderUpiId;
+    private String receiverUpiId;
+    private BigDecimal amount;
+    private TransactionStatus status;
+    private TransactionType type;
+    private String remarks;
+    private String npciResponseCode;
+    private String npciResponseMessage;
+    private LocalDateTime createdAt;
+    private LocalDateTime completedAt;
+}

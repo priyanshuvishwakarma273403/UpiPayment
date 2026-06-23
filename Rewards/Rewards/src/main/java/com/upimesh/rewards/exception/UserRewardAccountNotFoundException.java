e3f69e28ba176165eaf127505dcbf90b230258a5
Priyanshu <pv254424@gmail.com>
2026-06-23 23:29:09 +0530

    feat: add UserRewardAccountNotFoundException custom exception class

---
+++ Rewards/Rewards/src/main/java/com/upimesh/rewards/exception/UserRewardAccountNotFoundException.java
@@ -0,0 +1,7 @@
+package com.upimesh.rewards.exception;
+
+public class UserRewardAccountNotFoundException extends RuntimeException {
+    public UserRewardAccountNotFoundException(String message) {
+        super(message);
+    }
+}

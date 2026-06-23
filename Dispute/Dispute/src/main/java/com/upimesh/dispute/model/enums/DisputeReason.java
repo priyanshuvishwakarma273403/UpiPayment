49b482c570a3f956f546f3a7dc292543c80f8096
Priyanshu <pv254424@gmail.com>
2026-06-23 23:16:39 +0530

    feat: add DisputeStatus enum to define dispute lifecycle states

---
+++ Dispute/Dispute/src/main/java/com/upimesh/dispute/model/enums/DisputeReason.java
@@ -0,0 +1,9 @@
+package com.upimesh.dispute.model.enums;
+
+public enum DisputeReason {
+    UNAUTHORIZED,
+    DUPLICATE,
+    ITEM_NOT_RECEIVED,
+    WRONG_AMOUNT,
+    TECHNICAL_ERROR
+}
---
+++ Dispute/Dispute/src/main/java/com/upimesh/dispute/model/enums/DisputeStatus.java
@@ -0,0 +1,11 @@
+package com.upimesh.dispute.model.enums;
+
+public enum DisputeStatus {
+    RAISED,
+    MERCHANT_NOTIFIED,
+    EVIDENCE_SUBMITTED,
+    UNDER_REVIEW,
+    RESOLVED_REFUND,
+    RESOLVED_DISMISSED,
+    ESCALATED
+}
